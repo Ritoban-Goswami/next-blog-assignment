2d0cdbd3733aa5e4f9b20df823decd177d5ab01d
Ritoban Goswami <dev.ritoban.goswami@gmail.com>
2024-05-30 20:21:39 +0530

    feat: unbookmarking and user feedback implementation

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { BookmarkIcon } from "./SVGIcons";
-import { motion } from "framer-motion";
+import { BookmarkIcon, CheckIcon } from "./SVGIcons";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Bookmark = ({ blogID }: { blogID: number }) => {
   const [isBookmarked, setIsBookmarked] = useState<string>("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState<boolean>(false);
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
@@ -17,34 +18,63 @@ const Bookmark = ({ blogID }: { blogID: number }) => {
   const handleBookmark = () => {
     if (typeof window !== "undefined" && window.localStorage) {
       const savedBlogs = localStorage.getItem("saved_blogs") ?? "";
-      const updatedSavedBlogs = savedBlogs
-        ? `${savedBlogs},${blogID}`
-        : `${blogID}`;
+      let updatedSavedBlogsArray = savedBlogs ? savedBlogs.split(",") : [];
 
+      if (updatedSavedBlogsArray.includes(blogID.toString())) {
+        // Unbookmark
+        updatedSavedBlogsArray = updatedSavedBlogsArray.filter(
+          (id) => id !== blogID.toString()
+        );
+      } else {
+        // Bookmark
+        updatedSavedBlogsArray.push(blogID.toString());
+
+        // Show success message only when bookmarking
+        setShowSuccessMessage(true);
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000);
+      }
+
+      const updatedSavedBlogs = updatedSavedBlogsArray.join(",");
       localStorage.setItem("saved_blogs", updatedSavedBlogs);
       setIsBookmarked(updatedSavedBlogs);
     }
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.8 }}
-      onClick={handleBookmark}
-    >
-      <BookmarkIcon
-        className={`w-5 h-5 hover:fill-darkforeground ${
-          isBookmarked.includes(blogID.toString()) && "fill-darkforeground"
-        }`}
-      />
-      {/* {showMessage && (
-        <div className="text-sm text-gray-500 mt-1">
-          Blog saved for later reading
-        </div>
-      )} */}
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+        onClick={handleBookmark}
+      >
+        <BookmarkIcon
+          className={`w-5 h-5 hover:fill-gray-800 hover:dark:fill-gray-200 ${
+            isBookmarked.includes(blogID.toString()) &&
+            "dark:fill-gray-200 fill-gray-800"
+          }`}
+        />
+      </motion.div>
+      <AnimatePresence>
+        {showSuccessMessage && (
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            className="fixed top-4 right-4 flex items-center max-w-xs p-4 text-light bg-[#f0f0f0] rounded border border-gray-200 dark:border-gray-600 dark:bg-[#1e1e1e]"
+            role="alert"
+          >
+            <CheckIcon className="w-6 h-6 stroke-current" />
+            <div className="ms-3 text-sm font-normal">
+              Bookmark added successfully.
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/components/SVGIcons.tsx
+++ src/components/SVGIcons.tsx
@@ -148,3 +148,29 @@ export function BookmarkIcon(props: SVGProps) {
     </svg>
   );
 }
+
+export function CheckIcon(props: SVGProps) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M4 12.6111L8.92308 17.5L20 6.5"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </g>
+    </svg>
+  );
+}
